13eedcd544e80c1b57ca5f24b420552564c67f36
John Phillips <jphillq@gmail.com>
2026-01-10 14:57:01 -0700

    Fix up bad merge

--- ai.el
+++ ai.el
@@ -65,10 +65,6 @@
 (use-package inheritenv
   :vc (:url "https://github.com/purcell/inheritenv" :rev :newest))
 
-;; for eat terminal backend:
-(use-package eat :ensure t)
-
-
 (use-package monet
   :vc (:url "https://github.com/stevemolitor/monet" :rev :newest))
 
@@ -85,12 +81,4 @@
 (use-package inheritenv
   :vc (:url "https://github.com/purcell/inheritenv" :rev :newest))
 
-(use-package claude-code :ensure t
-  :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
-  :config
-   (claude-code-mode)
-   :bind-keymap ("C-c c" . claude-code-command-map))
-  ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
-  :bind
-  (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode)))
  
